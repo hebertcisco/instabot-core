44e802e9713b02cb0c0f65e79ecb5d143bcb4fb2
hebertcisco <mrrabbitomg@gmail.com>
2021-09-13 19:16:26 -0300

    feat: :art:

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { commentService } from './core/comment.service';
-export const comment  = commentService;
-export const core  = commentService;
- const instabot  = {
-   comment: commentService
- }
+import { getEmoji } from './services/getEmoji';
+import { randomInt, sleep } from './utils';
+
+export { commentService } from './core/comment.service';
+export * as comment from './core/comment.service';
+export * as core from './core/comment.service';
+export { getEmoji } from './services/getEmoji';
+export { randomInt, sleep } from './utils';
+
+const instabot = {
+  comment: commentService,
+  getEmoji: getEmoji,
+  randomInt: randomInt,
+  sleep: sleep
+}
 export default instabot
