49279bc2193ca92e7848fe9e003abe8f6efe41b5
hebertcisco <hebertcisco@outlook.com>
2022-04-10 16:28:38 +0000

    v1.0.5

--- example/index.js
+++ example/index.js
@@ -1,7 +1,12 @@
-import { commentService } from 'instabot-core';
+const { commentService } = require('../lib');
 
-((async() => { 
-    await commentService({
-            
-    })
-}))()
+const { IG_LINK, PHRASE_AUTHOR, IG_USERNAME, IG_PASSWORD } = process.env;
+
+(async () => {
+  await commentService({
+    link: IG_LINK,
+    author: PHRASE_AUTHOR,
+    loginInstagram: IG_USERNAME,
+    passwordInstagram: IG_PASSWORD,
+  });
+})();
